b1c3b324328e8b3385317ebb0b2be8a17ec02829
SofiTosto <101809134+SofiTosto@users.noreply.github.com>
2022-05-19 10:43:09 -0300

    Initial commit

---
+++ Assets/objectmovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class objectmovement : MonoBehaviour
+{
+    public GameObject micubo;
+    public Vector3 movementvector;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Mathf.Abs(micubo.transform.position.x) < 8) {
+
+            micubo.transform.position += movementvector;
+        }
+        
+    }
+}
---
+++ Assets/objectpositon.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class objectpositon : MonoBehaviour
+{
+    public Vector3 newposition;
+    public GameObject micubo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        micubo.transform.position = newposition;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
